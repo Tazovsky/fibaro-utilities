443b1d5341bad7e31a95870db75471e23ee4519f
Kamil Foltynski <kamil@foltynski.com>
2023-09-08 11:20:28 +0200

    feat: added battery check scene

---
+++ scenes/BatteryCheck-v0.1.0.lua
@@ -0,0 +1,47 @@
+-- Version 0.1.0 --------------------------------------------
+-- Author: Kamil Foltyński ----------------------------------
+-- Setup mandatory parameters -------------------------------
+
+local userID = {2} -- email to user
+local threshold = 20 -- in %
+
+-------------------------------------------------------------
+
+function getLowBatteryDevices(threshold)
+
+    local devices = hub.getDevicesID(
+        {
+            interface = "battery", 
+            enabled = true,
+            visible = true
+        }
+    )
+    local emailMsg = 'Devices with low battery:\n\n'
+    
+    if devices and type(devices) == "table" then
+        for i, deviceId in ipairs(devices) do
+        
+            local device = api.get("/devices/" .. tostring(deviceId))
+            if tonumber(device.properties.batteryLevel) <= tonumber(threshold) then
+
+                emailMsg = emailMsg .. string.format(
+                    "%s%%, ID: %s, %s, %s;\n",
+                    device.properties.batteryLevel, device.id, device.name, fibaro.getRoomNameByDeviceID(deviceId)
+                )
+            end
+        end
+        return emailMsg
+    else
+        fibaro.debug("No devices found.")
+    end
+end
+
+local out = getLowBatteryDevices(threshold)
+
+if out then
+    print(out)
+    --hub.alert('email', userID, out, false, '')
+    fibaro.call(userID, "sendEmail", "Fibaro: low battery!", out)
+end
+
+
